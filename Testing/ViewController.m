f7fca860dabf3249795f5b087bb0814e7c15f5cc
Patricia Springer <pattyspringer@macmartin-2.attlocal.net>
2016-10-26 13:44:43 -0700

    Initial Commit

---
+++ Testing/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Testing
+//
+//  Created by Patricia Springer on 10/26/16.
+//  Copyright © 2016 Daniel Springer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
